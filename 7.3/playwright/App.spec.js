04c15b93581186ae882039d07d8659c700eb2b4b
zaksignu <zaksignu@gmail.com>
2022-08-05 14:08:16 +0300

    final variant

--- 7.3/playwright/App.spec.js
+++ 7.3/playwright/App.spec.js
@@ -1,22 +1,22 @@
-// const { test, expect } = require("@playwright/test");
+const { test, expect } = require("@playwright/test");
 
-// test("test", async ({ page }) => {
-//   // Go to https://netology.ru/free/management#/
-//   await page.goto("https://netology.ru/free/management#/");
+test("test", async ({ page }) => {
+  // Go to https://netology.ru/free/management#/
+  await page.goto("https://netology.ru/free/management#/");
 
-//   // Click a
-//   await page.click("a");
-//   await expect(page).toHaveURL("https://netology.ru/");
+  // Click a
+  await page.click("a");
+  await expect(page).toHaveURL("https://netology.ru/");
 
-//   // Click text=Учиться бесплатно
-//   await page.click("text=Учиться бесплатно");
-//   await expect(page).toHaveURL("https://netology.ru/free");
+  // Click text=Учиться бесплатно
+  await page.click("text=Учиться бесплатно");
+  await expect(page).toHaveURL("https://netology.ru/free");
 
-//   page.click("text=Бизнес и управление");
+  page.click("text=Бизнес и управление");
 
-//   // Click text=Как перенести своё дело в онлайн
-//   await page.click("text=Как перенести своё дело в онлайн");
-//   await expect(page).toHaveURL(
-//     "https://netology.ru/programs/kak-perenesti-svoyo-delo-v-onlajn-bp"
-//   );
-// });
+  // Click text=Как перенести своё дело в онлайн
+  await page.click("text=Как перенести своё дело в онлайн");
+  await expect(page).toHaveURL(
+    "https://netology.ru/programs/kak-perenesti-svoyo-delo-v-onlajn-bp"
+  );
+});
